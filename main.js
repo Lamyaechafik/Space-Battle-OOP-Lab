0a4b8464876afeafa17e589e7bf2bb76917b6d44
Lamyae Chafik <lamyaechafik@Lamyaes-MBP.lan>
2023-07-12 09:44:57 -0400

    updated

--- main.js
+++ main.js
@@ -1,120 +1,56 @@
-class Ship {
-	constructor(hull, firepower, accuracy, name) {
-		this.hull = hull;
-		this.firepower = firepower;
-		this.accuracy = accuracy;
-		this.name = name;
-	}
+console.log('window is loaded');
 
-	// Attack the other ship
-	attack(otherShip) {
-		if (this.hull <= 0 || otherShip.hull <= 0) return;
+// Objects
 
-		if (Math.random() < this.accuracy) {
-			otherShip.hull -= this.firepower;
-			console.log(
-				`${this.name} hit ${otherShip.name}`,
-				`Hull: ${otherShip.hull}`
-			);
-		} else
-			console.log(
-				`${this.name} missed!`,
-				`${otherShip.name} Hull: ${otherShip.hull}`
-			);
+const hero = {
+    hull: 20,
+    firepower: 5,
+    accuracy: .7,
+    isAlive: true,
+    attack(target) {
+        let ranNum = Math.random();
+        console.log(`Accuracy threshold is ${ranNum}`);
+        if (ranNum < this.accuracy) {
+            console.log(`It's a direct hit!! Well done Capitan!`);
+            target.hull = target.hull - this.firepower;
+            console.log(`alien has ${target.hull} hull points left.`);
+            if (target.hull <= 0) {
+                target.isAlive = false;
+                console.log(`Alien ship is destroyed you have saved the Universe from complete destruction!!`);
 
-		if (otherShip.hull <= 0) {
-			console.log(`%c${otherShip.name} ship is destoryed!`, "color: red;");
-			console.log("");
-		}
-	}
+            }
+        } else {
+            console.log(`you missed!`);
+        }
+    }
 }
+const alien = {
+    hull: Math.round((Math.random() * (6 - 3)) + 3),
+    firepower: Math.round((Math.random() * (4 - 2)) + 2),
+    accuracy: (Math.random() * (.8 - .6)) + .6, //for later figure out a way to make it to the hundredth?
+    isAlive: true,
+    attack(target) {
+        let ranNum = Math.random();
+        console.log(`Accuracy threshold is ${ranNum}`);
+        if (ranNum < this.accuracy) {
+            console.log(`You've been hit!`);
+            target.hull = target.hull - this.firepower;
+            console.log(`hero has ${target.hull} hull points left.`);
+            if (target.hull <= 0) {
+                target.isAlive = false;
+                console.log(`You have died, your people will now be used as cattle for the CRAVERSSS!!! GRRZZT ZOIK!`);
 
-const alienShips = []; // Array to store the instances
-const numberOfShips = 6; // Number of instances you want to create
-
-// Set a defined number of alien ships with varied stats
-for (let i = 0; i < numberOfShips; i++) {
-	let hull = Math.floor(Math.random() * 4) + 3;
-	let firepower = Math.floor(Math.random() * 3) + 2;
-	let accuracy = Math.random() * 0.2 + 0.6;
-
-	const alienShip = new Ship(
-		hull,
-		firepower,
-		accuracy,
-		getRandomAlienShipName()
-	); // Create a new instance
-	alienShips.push(alienShip); // Add the instance to the array
+            }
+        } else {
+            console.log(`You dodged the attack!`);
+        }
+    }
 }
-
-const USS_Assembly = new Ship(20, 5, 0.7, "USS Assembly");
-
-let battleOver = false;
-
-// Function to create a promise that resolves when the attack button is pressed
-const waitForAttack = () => {
-	return new Promise((resolve) => {
-		const attack = document.querySelector(".attack-button");
-		attack.onclick = () => {
-			resolve(); // Resolve the promise when the attack button is pressed
-		};
-	});
-};
-
-// Battle until my ship is destroyed or all enemy ships are destroyed
-(async () => {
-	for (let i = 0; i < alienShips.length; i++) {
-		const alienShip = alienShips[i];
-
-		await waitForAttack(); // Wait for the attack button to be pressed
-		const retreat = document.querySelector(".retreat-button");
-		retreat.onclick = () => {
-			battleOver = true;
-			console.log("%cUSS Assembly has retreated", "color: yellow;");
-		};
-
-		if (battleOver) {
-			break;
-		}
-
-		while (USS_Assembly.hull > 0 && alienShip.hull > 0) {
-			USS_Assembly.attack(alienShip);
-			alienShip.attack(USS_Assembly);
-			if (USS_Assembly.hull <= 0) {
-				console.log("%cYou Lose.", "color: red;");
-				battleOver = true;
-				break;
-			}
-		}
-
-		if (battleOver) {
-			break;
-		}
-
-		if (i === alienShips.length - 1) {
-			console.log("%cYou Win.", "color: green;");
-			battleOver = true;
-			break;
-		}
-	}
-})();
-
-function getRandomAlienShipName() {
-	const name = [
-		"Zorthak",
-		"Xylophor",
-		"Vulcanix",
-		"Quasarion",
-		"Nebulor",
-		"Stellarium",
-		"Xor'gath",
-		"Valkyron",
-		"Zephyrion",
-		"Astronix",
-		"Lyra'xis",
-		"Nocturna",
-	];
-
-	const randomName = name[Math.floor(Math.random() * name.length)];
-	return randomName;
+const battle = (player, computer) => {
+    while (player.isAlive && computer.isAlive) {
+        player.attack(computer);
+        if (computer.isAlive) {
+            computer.attack(player);
+        }
+    }
 }
